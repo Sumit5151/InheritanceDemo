8cb6e03417f4f4c7e4ed6e41b69ad8bc622af897
Sumit5151 <sumitshitole5151@gmail.com>
2023-03-30 08:35:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+
+using InheritanceDemo;
+
+
+Child2 c2 = new Child2();
+c2.function3();
+c2.function4();
+c2.function7();
+c2.function8();
+
+
+Child1 c1 = new Child1();
+//c.function1();
+//c.function2();
+c1.function3();
+c1.function4();
+c1.function5();
+c1.function6();
+
+
+
+
+Parent p = new Parent();
+//p.function1();
+//p.function2();
+p.function3();
+p.function4();
+//p.function5();  
+
+
+
+//GrandParent gp = new GrandParent();
+
+//gp.function1();
+//gp.function2();
+//gp.function3();
+
